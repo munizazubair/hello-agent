9a4d81b33246aa535addb293c3041b94fb028e2c
munizazubair <munizazubairkhan@gmail.com>
2025-05-31 03:35:29 +0500

    add

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-print("hello agent")
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents.run import RunConfig
+import asyncio
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is missing! Please add it in the .env file like: GEMINI_API_KEY=your_key_here")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+
+async def main():
+    agent = Agent(
+        name="Assistant",
+        instructions="You are helpful Assistant.",
+        model=model
+    )
+
+    result = await Runner.run(agent, "I want to cook Biryani. Please provide a complete list of ingredients and detailed step-by-step instructions to prepare it at home.", run_config=config)
+    print(result.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
